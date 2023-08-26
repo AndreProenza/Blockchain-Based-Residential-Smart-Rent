cd5b8f60c86aff04f69046892ddbdab43ba3caeb
AndreProenza <proenza.1999@gmail.com>
2023-08-26 12:32:20 +0100

    Updated redux and deleted context

--- Project-Tech/Frontend/unlockit/src/redux/store.js
+++ Project-Tech/Frontend/unlockit/src/redux/store.js
@@ -3,8 +3,12 @@ import userLoginReducer from '../features/userLoginSlice'
 import userReducer from '../features/userSlice'
 import contractReducer from '../features/contractSlice'
 import propertyReducer from '../features/propertySlice'
+import propertyPhotoReducer from '../features/propertyPhotoSlice'
 import advertiseReducer from '../features/advertiseSlice'
 import listingsReducer from '../features/listingsSlice'
+import rentalInfoReducer from '../features/rentalInfoSlice'
+import proposalReducer from '../features/proposalSlice'
+
 
 export default configureStore({
     reducer: {
@@ -12,7 +16,10 @@ export default configureStore({
         user: userReducer,
         contract: contractReducer,
         property: propertyReducer,
+        propertyPhoto: propertyPhotoReducer,
         advertise: advertiseReducer,
         listings: listingsReducer,
+        rentalInfo: rentalInfoReducer,
+        proposal: proposalReducer,
     },
 })
