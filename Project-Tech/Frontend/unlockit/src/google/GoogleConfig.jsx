d597d66b790f0258393e320d51e20c5935348fa3
AndreProenza <proenza.1999@gmail.com>
2023-09-05 10:14:17 +0100

    create google signin / login config file

---
+++ Project-Tech/Frontend/unlockit/src/google/GoogleConfig.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+class GoogleConfig extends React.Component {
+    
+    static googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+}
+
+export default GoogleConfig;
